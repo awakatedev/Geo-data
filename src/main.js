90d27ad6cdc573084cdbbe5dab8ffc40316ea8b1
jhonathan <jhonathanjhs10@gmail.com>
2021-02-03 01:31:37 -0400

    ready the fetch and starting to apply styles, 2commit os the project

--- src/main.js
+++ src/main.js
@@ -19,3 +19,56 @@ function getResults(query) {
     })
     .then(displayResults);
 }
+
+function displayResults(weather) {
+  let city = document.querySelector(".location .city");
+  city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+  let now = new Date();
+  let date = document.querySelector(".location .date");
+  date.innerText = dateBuilder(now);
+
+  let temp = document.querySelector(".current .temp");
+  hilow.innerHTML = `${Math.round(weather.main.temp)}<span>ºc</span>`;
+
+  let weather_el = document.querySelector(".current .weather");
+  weather_el.innerText = weather.weather[0].main;
+
+  let hilow = document.querySelector(".hi-low");
+  hilow.innerText = `${Math.round(weather.main.temp_min)}ºc / ${Math.round(
+    weather.main.temp_max
+  )}ºc`;
+}
+
+function dateBuilder(d) {
+  let months = [
+    "Junuary",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "july",
+    "Agust",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let days = [
+    "Sunday",
+    "Moday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let day = days[d.getDay()];
+  let date = d.getDate();
+  let month = months[d.getMonth()];
+  let year = d.getFullYear();
+
+  return `${day} ${date} ${month} ${year}`;
+}
