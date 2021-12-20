75176f40b19a4be594c1d41ae2a1f216510db23a
Jhonathan <jhonathanjhs@gmail.com>
2021-12-20 18:58:05 +0000

    Starting

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 //import ip from "../routes/api.js";
 
 const Home = () => {
-    const IP = async () => {
+  const IP = async () => {
     try {
       const response = await fetch(`https://api64.ipify.org?format=json`);
       const ip = await response.json();
@@ -11,21 +11,21 @@ const Home = () => {
     } catch (e) {
       console.log(e);
     }
-};
-useEffect(() => {
-  IP()
-}, []);
+  };
+  useEffect(() => {
+    IP();
+  }, []);
   /* useEffect(() => {
     fetch("https://api64.ipify.org?format=json")
     .then(response => response.json())
     .then(data => console.log(data))
   },[]);*/
-  return(
-  <div>
-    <section className="hero">
-      <h1>h</h1>
-    </section>
-  </div>
-);
+  return (
+    <div>
+      <section className="hero">
+        <h1>h</h1>
+      </section>
+    </div>
+  );
 };
 export default Home;
